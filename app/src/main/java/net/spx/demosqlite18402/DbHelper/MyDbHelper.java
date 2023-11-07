f21e50ab513ad0b9ff8e78b2f018c8cc19e15ec9
snvn1511 <spxedu@gmail.com>
2023-11-07 10:25:53 +0700

    Initial commit

---
+++ app/src/main/java/net/spx/demosqlite18402/DbHelper/MyDbHelper.java
@@ -0,0 +1,41 @@
+package net.spx.demosqlite18402.DbHelper;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MyDbHelper extends SQLiteOpenHelper {
+    static String DB_NAME = "ql_banhang";
+    static int DB_VERSION = 1;
+
+    public MyDbHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        // viết lệnh tạo bảng ở đây
+        String sql_tb_cat = "CREATE TABLE tb_cat ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT UNIQUE NOT NULL );";
+        sqLiteDatabase.execSQL( sql_tb_cat ); // thực hiện tạo bảng
+
+        String sql_tb_product = "CREATE TABLE tb_product ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, price INTEGER DEFAULT (0) NOT NULL,id_cat INTEGER NOT NULL CONSTRAINT fk_pro_cat REFERENCES tb_cat (id) );";
+        sqLiteDatabase.execSQL( sql_tb_product ); // thực hiện tạo bảng
+
+        // Thêm dữ lệu DEMO
+        sqLiteDatabase.execSQL("INSERT INTO tb_cat ('name') VALUES('Tivi'), ('Tủ lạnh'), ('Máy giặt') ");
+        
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        if(i<i1){
+            sqLiteDatabase.execSQL(" DROP TABLE IF EXISTS tb_product ");
+            sqLiteDatabase.execSQL(" DROP TABLE IF EXISTS tb_cat ");
+            onCreate( sqLiteDatabase ); // tạo lại bảng mới.
+        }
+
+
+    }
+}
---
+++ app/src/main/java/net/spx/demosqlite18402/MainActivity.java
@@ -0,0 +1,14 @@
+package net.spx.demosqlite18402;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
